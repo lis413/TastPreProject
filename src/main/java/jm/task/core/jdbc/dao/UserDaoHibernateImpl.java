5e977f5c227d027329af4cb49c31e7c6c23be0bd
lis154 <lis154@mail.ru>
2020-06-23 14:49:48 +0300

    commit hiber

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,42 +1,65 @@
 package jm.task.core.jdbc.dao;
 
+
 import jm.task.core.jdbc.model.User;
+import org.hibernate.Query;
+import org.hibernate.Session;
 
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
+
+    private Session session;
+
     public UserDaoHibernateImpl() {
 
     }
 
+    public UserDaoHibernateImpl(Session session){
+        this.session = session;
+    }
+
+
+
 
     @Override
     public void createUsersTable() {
+        String hql = "create table if not exists users (id bigint auto_increment, name varchar(256), lastName varchar(256), age int, primary key (id))";
+        Query query = session.createQuery(hql);
+        query.executeUpdate();
 
     }
 
     @Override
     public void dropUsersTable() {
-
+        Query query = session.createQuery("drop table users");
+        query.executeUpdate();
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        session.save(new User(name, lastName, age));
     }
 
     @Override
     public void removeUserById(long id) {
-
+        String hql = "delete from users where id = '" + id + "'";
+        Query query = session.createQuery(hql);
+        query.executeUpdate();
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+
+        String hql = "from users";
+        Query query = session.createQuery(hql);
+        List<User> list = query.list();
+        return list;
     }
 
     @Override
     public void cleanUsersTable() {
-
+        Query query = session.createQuery("DELETE FROM users");
+        query.executeUpdate();
     }
 }
